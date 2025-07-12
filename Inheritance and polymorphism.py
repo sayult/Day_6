69e235639617c92b3b98ff81383692119084e1ea
sayult <2948174604@qq.com>
2025-07-12 13:58:56 +0800

    多重继承

--- Inheritance and polymorphism.py
+++ Inheritance and polymorphism.py
@@ -31,8 +31,29 @@ def introduce_person(person):
 person1 = Person('王', 21)
 teacher1 = Teacher('张教授', 45, '计算机',11451)
 
-introduce_person(person1)
-introduce_person(teacher1)
+
+
+class Researcher:
+
+    def __init__(self, research_area):
+        self.research_area = research_area
+        self.publications = []
+
+    def publish_paper(self, paper_title):
+        self.publications.append(paper_title)
+        print(f'新论文发表：《{paper_title}》')
+
+class Professor(Teacher, Researcher): #多重继承
+    def __init__(self, name, age, department, employee_id, research_area):
+
+        Teacher.__init__(self, name, age, department, employee_id)
+        Researcher.__init__(self, research_area)
+        self.title = '教授'
+
+    def introduce(self):
+        print(f'我是{self.department}系的{self.title}{self.name}')
+        print(f'研究方向：{self.research_area}')
+        print(f'已发表论文{len(self.publications)}篇')
 
 
 
